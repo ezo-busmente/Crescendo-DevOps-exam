2a0a8332c856e32a0d980f51abd04f3cfb5c7213
Ezekiel Aine Busmente <zezekiel-a.busmente@gfitftlapt04273.ad.mynt.xyz>
2026-09-14 23:06:47 +0800

    fix: update vm sku to D2s_v5 and specify modern appgw ssl policy

--- loadbalancing.tf
+++ loadbalancing.tf
@@ -19,6 +19,11 @@ resource "azurerm_application_gateway" "appgw" {
     capacity = 1
   }
 
+  ssl_policy {
+    policy_type = "Predefined"
+    policy_name = "AppGwSslPolicy20220101"
+  }
+
   gateway_ip_configuration {
     name      = "appgw-ip-config"
     subnet_id = azurerm_subnet.appgw.id
