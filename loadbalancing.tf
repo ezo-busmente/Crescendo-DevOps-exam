896c5aeabfe9fe06a85a27d2638cf5f10da1fe4f
Ezekiel Aine Busmente <zezekiel-a.busmente@gfitftlapt04273.ad.mynt.xyz>
2026-09-14 22:58:06 +0800

    fix: change vm sku size and update appgw probe settings

--- loadbalancing.tf
+++ loadbalancing.tf
@@ -50,12 +50,13 @@ resource "azurerm_application_gateway" "appgw" {
   }
 
   backend_http_settings {
-    name                  = "backend-http-settings"
-    cookie_based_affinity = "Disabled"
-    port                  = 80
-    protocol              = "Http"
-    request_timeout       = 20
-    probe_name            = "health-probe"
+    name                                = "backend-http-settings"
+    cookie_based_affinity               = "Disabled"
+    port                                = 80
+    protocol                            = "Http"
+    request_timeout                     = 20
+    probe_name                          = "health-probe"
+    pick_host_name_from_backend_address = true
   }
 
   http_listener {
