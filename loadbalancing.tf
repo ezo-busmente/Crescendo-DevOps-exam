728aed9f786d0a1ae3e976aa0d0d728643d73662
Ezekiel Aine Busmente <zezekiel-a.busmente@gfitftlapt04273.ad.mynt.xyz>
2026-09-14 22:49:20 +0800

    fix: remove front door due to free subscription limits

--- loadbalancing.tf
+++ loadbalancing.tf
@@ -35,7 +35,7 @@ resource "azurerm_application_gateway" "appgw" {
   }
 
   backend_address_pool {
-    name = "backend-pool"
+    name         = "backend-pool"
     ip_addresses = [azurerm_network_interface.vm_nic.private_ip_address]
   }
 
@@ -73,63 +73,4 @@ resource "azurerm_application_gateway" "appgw" {
     backend_http_settings_name = "backend-http-settings"
     priority                   = 100
   }
-}
-
-resource "azurerm_cdn_frontdoor_profile" "fd" {
-  name                = "crescendo-fd-profile"
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "Standard_AzureFrontDoor"
-}
-
-resource "azurerm_cdn_frontdoor_endpoint" "fd_endpoint" {
-  name                     = "crescendo-fd-endpoint-${random_string.suffix.result}"
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd.id
-}
-
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
-  upper   = false
-}
-
-resource "azurerm_cdn_frontdoor_origin_group" "fd_origin_group" {
-  name                     = "appgw-origin-group"
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd.id
-  session_affinity_enabled = false
-
-  load_balancing {
-    sample_size                 = 4
-    successful_samples_required = 3
-  }
-
-  health_probe {
-    path     = "/"
-    protocol = "Http"
-    interval_in_seconds = 100
-  }
-}
-
-resource "azurerm_cdn_frontdoor_origin" "fd_origin" {
-  name                           = "appgw-origin"
-  cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
-  enabled                        = true
-  host_name                      = azurerm_public_ip.appgw_ip.ip_address
-  http_port                      = 80
-  https_port                     = 443
-  origin_host_header             = azurerm_public_ip.appgw_ip.ip_address
-  priority                       = 1
-  weight                         = 1000
-  certificate_name_check_enabled = false
-}
-
-resource "azurerm_cdn_frontdoor_route" "fd_route" {
-  name                          = "default-route"
-  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.fd_endpoint.id
-  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
-  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.fd_origin.id]
-  supported_protocols           = ["Http", "Https"]
-  patterns_to_match             = ["/*"]
-  forwarding_protocol           = "HttpOnly"
-  link_to_default_domain        = true
-  https_redirect_enabled        = true
 }
